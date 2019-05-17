e5712039d52c5c90358de70083ee4bd37135360e
Ntinos <antinos@deloitte.gr>
2019-05-17 11:53:58 +0300

    Fixed error popup

--- force-app/main/default/lwc/oppComponent/oppComponent.js
+++ force-app/main/default/lwc/oppComponent/oppComponent.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable vars-on-top */
 import { LightningElement, track, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
-import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
+import { registerListener, unregisterAllListeners } from 'c/pubsub';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import findOpportunities from '@salesforce/apex/FindOpp.searchOpps';
 
 const columns = [
@@ -13,13 +16,35 @@ const columns = [
 export default class DatatableWithInlineEdit extends LightningElement {
     @track columns = columns;
     @track rowOffset = 0;
-    searchKey;
+    @track opportunities;
+    @track errror;
+    @track searchKey;
 
-    @wire(CurrentPageReference) 
+    @wire(CurrentPageReference)
     pageRef;
 
-    @wire(findOpportunities, { searchOppByAcc: '$searchKey' }) 
-    opportunities;
+    @wire(findOpportunities, { searchOppByAcc: '$searchKey' })
+    wiredOppRetrieval({ error, data }) {
+        if (data) {
+            // eslint-disable-next-line no-console
+            console.log("inside if data");
+            this.opportunities = data;
+            this.error = undefined;
+            if(this.isEmpty(this.opportunities)){
+                console.log("inside is empty");
+                this.handleError();
+                this.searchKey = " ";
+               
+            }
+
+
+        } else if(error){
+            this.error = error;
+            this.opportunities = undefined;
+            
+        }
+
+    }
 
     connectedCallback() {
         registerListener('findOpportunitiesEventName', this.handleSearchKey, this);
@@ -31,6 +56,26 @@ export default class DatatableWithInlineEdit extends LightningElement {
 
     handleSearchKey(searchKeyParam) {
         this.searchKey = searchKeyParam;
+    }
+
+    handleError() {
+        // eslint-disable-next-line no-console
+        console.log("inside handleerror");
+        const showError = new ShowToastEvent({
+            title: "No Opportunities found",
+            message: "There are no opportunities for the selected account",
+            variant: 'Error',
+        });
+        this.dispatchEvent(showError);
+
+    }
+
+     isEmpty(obj) {
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key))
+                return false;
+        }
+        return true;
     }
 
 }
