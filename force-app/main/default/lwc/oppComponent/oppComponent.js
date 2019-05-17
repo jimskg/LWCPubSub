bde8e7b47dd2520a8386781d6a67873212c0c4f3
Kartsonakis Mademlis <dkartsonakismademlis@deloitte.gr>
2019-05-17 12:08:47 +0300

    fixed conflicts

--- force-app/main/default/lwc/oppComponent/oppComponent.js
+++ force-app/main/default/lwc/oppComponent/oppComponent.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable vars-on-top */
 import { LightningElement, track, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
-import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
+import { registerListener, unregisterAllListeners } from 'c/pubsub';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import findOpportunities from '@salesforce/apex/FindOpp.searchOpps';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -14,25 +17,38 @@ const columns = [
 export default class DatatableWithInlineEdit extends LightningElement {
     @track columns = columns;
     @track rowOffset = 0;
-    searchKey;
+
     @track opportunities;
-    @wire(CurrentPageReference) 
+    @track errror;
+    @track searchKey;
+
+    @wire(CurrentPageReference)
     pageRef;
     stringOpp;
 
     // @wire(findOpportunities, { searchOppByAcc: '$searchKey' }) 
     // opportunities;
 
-    @wire(findOpportunities, { searchOppByAcc: '$searchKey' }) 
-    LoadOpps(result) {
-        this.opportunities = JSON.parse(JSON.stringify(result)).data; 
-        //console.log(Object.keys());
-        console.log(JSON.parse(JSON.stringify(result)).data);
-        if (true){
-            this.handleError();
+    @wire(findOpportunities, { searchOppByAcc: '$searchKey' })
+    wiredOppRetrieval({ error, data }) {
+        if (data) {
+            // eslint-disable-next-line no-console
+            console.log("inside if data");
+            this.opportunities = data;
+            this.error = undefined;
+            if (this.isEmpty(this.opportunities)) {
+                console.log("inside is empty");
+                this.handleError();
+                this.searchKey = " ";
+
+            }
+
+
+        } else if (error) {
+            this.error = error;
+            this.opportunities = undefined;
+
         }
-        
-    }
 
     }
 
@@ -46,6 +62,26 @@ export default class DatatableWithInlineEdit extends LightningElement {
 
     handleSearchKey(searchKeyParam) {
         this.searchKey = searchKeyParam;
+    }
+
+    handleError() {
+        // eslint-disable-next-line no-console
+        console.log("inside handleerror");
+        const showError = new ShowToastEvent({
+            title: "No Opportunities found",
+            message: "There are no opportunities for the selected account",
+            variant: 'Error',
+        });
+        this.dispatchEvent(showError);
+
+    }
+
+    isEmpty(obj) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key))
+                return false;
+        }
+        return true;
     }
 
 }
